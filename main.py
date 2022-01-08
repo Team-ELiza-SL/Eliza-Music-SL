19793e8894b44a953243efd5ca92e538ec4b3e2b
Sehath Perera <93459498+PereraSehath@users.noreply.github.com>
2022-01-08 13:39:53 +0530

    prepare

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from pytgcalls import idle
+from callsmusic import run
+from handlers import __version__
+from pyrogram import Client as Bot
+from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+
+
+response = requests.get(BG_IMAGE)
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
+
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="handlers"),
+)
+
+print(f"[INFO]: ELIZA MUSIC v{__version__} STARTED !")
+
+bot.start()
+run()
+idle()
